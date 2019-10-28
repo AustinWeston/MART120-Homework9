dd43db839b93b448725ca8d55719ed67bd0a95fb
AustinWeston <36024664+AustinWeston@users.noreply.github.com>
2019-10-28 16:15:07 -0600

    Homework 9

---
+++ CreativeCodingHomework9/Sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(0,220,0);
+  fill(0,0,255);
+  rect(150,220,20,110);
+  rect(210,220,20,110);
+  fill(255,0,0);
+  ellipse(190,180,120,150);
+  line(140,170,120,100);
+  line(240,170,260,100);
+  fill(197, 140, 133)
+  triangle(140,40,190,120,240,40);
+  line(160,50,148,10);
+  line(170,50,158,10);
+  line(180,50,168,10);
+  line(190,50,178,10);
+  line(200,50,188,10);
+  line(210,50,198,10);
+  line(220,50,208,10);
+  point(190,70);
+  fill(0,0,0);
+  square(175,60,5);
+  square(200,60,5);
+  rect(183,85,15,3);
+  textSize(16);
+  text('Austin Cook',280,395);
+  textSize(12);
+  text('Pizza Face',0,10)
+  
+  
+}
